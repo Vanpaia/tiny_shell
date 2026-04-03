fa66b505efbfefa2766c6513df5bef01dde4b483
hagen <h.schlotzhauer@gmail.com>
2026-04-03 09:17:27 +0200

    small change in lexer character parsing

--- utils/lexer.c
+++ utils/lexer.c
@@ -2,36 +2,95 @@
 #include "../config.h"
 
 #include <stdio.h>
+#include <string.h>
+
+
+/* This function resets the shell command struct to be used for the next input */
+int reset_token(struct ShellToken *c) {
+	memset(c, 0, sizeof(struct ShellToken));
+	return 0;
+}
 
 /* This first lexer version assumes ASCII input only (pointer arithmatic) and should be upgraded to UTF-8 in due time */
 /* It also only reads tokens that have been pre-processed by strtok, as such it also returns whether a comment is still open */
-int tokenize(char* c, int* cmt) {
+int tokenize(char* c) {
 	char str_buffer[(BUFFER_SIZE / 2)];
-	char token_type[4];
+
+	struct ShellToken token = {0};
+
+	while (*c != '\0') {
+		/* Check for the type of token based on the signals (- for flags, " for quote, in combination with whether we are already in a comment (cmt) ) */
+		if (strcmp(c, " ") == 0 || strcmp(c, ";") == 0) {
+			if (token.string[0] != '\0') {
+				printf("%s - %s\n", token.type, token.string);
+				reset_token(&token);
+			}
+			c++;
+		}
+		if (token.type[0] == '\0') {
+			if (strcmp(c, "-") == 0) {
+				strncpy(token.type, "flg", 3);
+				c++;
+				continue;
+			} else if (strcmp(c, "\"") == 0) {
+				strncpy(token.type, "qte", 3);
+				c++;
+				continue;
+			} else {
+				strncpy(token.type, "cmd", 3);
+				c++;
+				continue;
+			}
+		}
+
+			strncpy(token.type, "cmt", 3);
+			token.type[3] = '\0';
+			if (strcmp(c, "\"") == 0) {
+				*cmt = 0;
+				c++;
+				continue;
+			} 
+		}
+		if (strcmp(c, "\"") != 0) {
+			str_buffer[strlen(str_buffer)] = *c;
+			str_buffer[strlen(str_buffer)+1] = '\0';
+			printf("%c", *c);
+		}
+		c++;
+	}
+	return 0;
+}
+
+
+/* This first lexer version assumes ASCII input only (pointer arithmatic) and should be upgraded to UTF-8 in due time */
+/* It also only reads tokens that have been pre-processed by strtok, as such it also returns whether a comment is still open */
+int parse(char* c, int* cmt) {
+	char str_buffer[(BUFFER_SIZE / 2)];
+	char token.type[4];
 
 	while (*c != '\0') {
 		/* Check for the type of token based on the signals (- for flags, " for comments, in combination with whether we are already in a comment (cmt) ) */
 		if (*cmt == 0) {
-			if (token_type[0] == '\0' && strcmp(c, "-") == 0) {
-				strncpy(token_type, "flg", 3);
-				token_type[3] = '\0';
+			if (token.type[0] == '\0' && strcmp(c, "-") == 0) {
+				strncpy(token.type, "flg", 3);
+				token.type[3] = '\0';
 				c++;
 				continue;
-			} else if (token_type[0] == '\0' && strcmp(c, "\"") == 0) {
-				strncpy(token_type, "cmt", 3);
-				token_type[3] = '\0';
+			} else if (token.type[0] == '\0' && strcmp(c, "\"") == 0) {
+				strncpy(token.type, "cmt", 3);
+				token.type[3] = '\0';
 				*cmt = 1;
 				c++;
 				continue;
-			} else if (token_type[0] == '\0') {
-				strncpy(token_type, "cmd", 3);
-				token_type[3] = '\0';
+			} else if (token.type[0] == '\0') {
+				strncpy(token.type, "cmd", 3);
+				token.type[3] = '\0';
 				c++;
 				continue;
 			}
 		} else if (*cmt == 1) {
-			strncpy(token_type, "cmt", 3);
-			token_type[3] = '\0';
+			strncpy(token.type, "cmt", 3);
+			token.type[3] = '\0';
 			if (strcmp(c, "\"") == 0) {
 				*cmt = 0;
 				c++;
--- utils/lexer.h
+++ utils/lexer.h
@@ -4,7 +4,16 @@
 #include "../config.h"
 #include "../models.h"
 
-/* This function will parse the user input for commands, flags, and others */
-int tokenize(char* c, int* cmt);
+/* This struct contains all the elements of a single token */
+struct ShellToken {
+	char string[BUFFER_SIZE];
+	char type[4];
+};
+
+/* This function will tokenize the user input to break it down into smaller parts to parse */
+int tokenize(char* c);
+
+/* This function will parse the tokens for commands, flags, and others */
+int parse(char* c, int* cmt);
 
 #endif
